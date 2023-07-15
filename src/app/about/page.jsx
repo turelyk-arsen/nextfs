69786ca25998fc52e5065b718d4452f441cd20ac
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-07-15 12:43:22 +0200

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css'
+
+// rafce snippet
+
+
+const About = () => {
+  return (
+    <div className={styles.container}>About</div>
+  )
+}
+
+export default About
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.container}>
+        <div>2023 MyApp. All rights reserved</div>
+        <div></div>
+    </footer>
+  )
+}
+
+export default Footer
