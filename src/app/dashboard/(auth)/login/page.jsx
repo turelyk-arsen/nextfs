6a26bbda6ba61341320ad90dc2d10ae95145ba13
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-07-17 14:09:07 +0200

    add login

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,10 +1,40 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
+  const session = useSession();
+  const router = useRouter();
+
+  console.log(session);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn("credentials", { email, password });
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+      </form>
       <button onClick={() => signIn("google")}>Login with Google</button>
     </div>
   );
